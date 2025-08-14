a4d57022859d256594580577c84e5669d40ae3bb
Teo Fumagalli <teo.fum@gmail.com>
2025-08-14 19:21:47 -0300

    add arg parsing for master

---
+++ master/include/args.h
@@ -0,0 +1,24 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+#include <stdint.h>
+
+#define MAX_PLAYERS 9
+
+typedef struct {
+  uint32_t width, height;
+
+  uint32_t delay;
+  uint32_t timeout;
+
+  int32_t seed;
+
+  const char *view;
+  const char *players[MAX_PLAYERS];
+} args_t;
+
+int parse_args(int argc, char *const *argv, args_t *args, const char **err);
+
+void free_args(args_t *args);
+
+#endif
