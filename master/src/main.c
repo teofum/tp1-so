dfd690e1755d49ccb4c510b01def6987ae8332d1
lolo-pb <lpizzutobeltran@gmail.com>
2025-08-20 17:27:18 -0300

    pipes de players mk1 NPI si anda,  y no se que hay que incluir,lo hago en hci

--- master/src/main.c
+++ master/src/main.c
@@ -23,6 +23,36 @@ int main(int argc, char **argv) {
   for (int i = 0; args.players[i] != NULL; i++)
     printf("Player %d: %s\n", i + 1, args.players[i]);
 
+  ///// pipe setup mk1 ////
+
+  int pipesw[2][MAX_PLAYERS]; // un array de pipes de read pipes de read
+  int pipesr[2][MAX_PLAYERS]; // un array de pipes de write
+
+  for (int i = 0; args.players[i] != NULL; i++) {
+    if (pipe(pipesw[i]) == -1 || pipe(pipesr[i]) == -1) {
+      return -1; // falla el creado del pipe
+    }
+  }
+  // a cada player va a haber que mandarle sus respectivos pipes de read y write
+
+  int children[MAX_PLAYERS];
+  for (int i = 0; args.players[i] != NULL; i++) {
+    int pid = fork();
+    if (pid == -1) {
+      return -1;
+    } // falla el fork
+
+    if (pid == 0) {
+      /// child proces // deberia correr el codigo del player
+      execve("player", args.players,
+             enviroment); // todo enviroment variables to pass
+    }
+    children[i] = pid; // parent guarda el pid de los nenes
+  }
+
+  /// en  este punto estan los pipes en pipesr/w y los hijos de players creados
+  /// todo: faltan  los pipes de la vista
+
   free_args(&args);
   return 0;
 }
