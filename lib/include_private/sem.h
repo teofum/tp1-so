221d53da10d140cf4db2dfa45b2cd3034018b38e
Teo Fumagalli <teo.fum@gmail.com>
2025-09-12 19:36:42 -0300

    wrap semaphore syscalls

---
+++ lib/include_private/sem.h
@@ -0,0 +1,47 @@
+/*
+ * Simple wrapper around the system semaphore APIs
+ * Intended to be easily replaceable on a different system
+ */
+
+#ifndef SEM_H
+#define SEM_H
+
+#include <semaphore.h>
+#include <stdint.h>
+
+// This typedef is obviously redundant, but the idea is you could eventually
+// change it on a system with a different named semaphore type
+typedef sem_t sem_t;
+
+typedef enum {
+  SEM_SCOPE_THREADS = 0, // Shared between threads of the same process
+  SEM_SCOPE_PROCS = 1,   // Shared between processes
+} sem_scope_t;
+
+// semaphore_ prefix is a little verbose but sem_ is taken for a lot of function
+// names
+
+/*
+ * Create a semaphore
+ */
+int semaphore_create(sem_t *sem, sem_scope_t scope, uint32_t initial_value);
+
+/*
+ * Destroy a semaphore
+ */
+int semaphore_destroy(sem_t *sem);
+
+// i'd rather have the sem_ prefix but those names are taken, sadge
+
+/*
+ * If the semaphore value is positive, decrease it and continue.
+ * If it is zero, blocks until there is a positive value.
+ */
+int semaphore_wait(sem_t *sem);
+
+/*
+ * Increment the semaphore value by 1.
+ */
+int semaphore_post(sem_t *sem);
+
+#endif
--- lib/src/game_sync.c
+++ lib/src/game_sync.c
@@ -1,30 +1,31 @@
-#include "game_sync.h"
+#include <game_sync.h>
+#include <sem.h>
 
 void game_sync_init(game_sync_t *game_sync, uint32_t n_players) {
   game_sync->read_count = 0;
 
   // Initialize semaphores
-  sem_init(&game_sync->view_should_update, 1, 0); // 0: view waits for master
-  sem_init(&game_sync->view_did_update, 1, 0);    // 0: master waits for view
+  semaphore_create(&game_sync->view_should_update, SEM_SCOPE_PROCS, 0);
+  semaphore_create(&game_sync->view_did_update, SEM_SCOPE_PROCS, 0);
 
-  sem_init(&game_sync->master_write_mutex, 1, 1); // 1: unlocked
-  sem_init(&game_sync->game_state_mutex, 1, 1);   // 1: unlocked
-  sem_init(&game_sync->read_count_mutex, 1, 1);   // 1: unlocked
+  semaphore_create(&game_sync->master_write_mutex, SEM_SCOPE_PROCS, 1);
+  semaphore_create(&game_sync->game_state_mutex, SEM_SCOPE_PROCS, 1);
+  semaphore_create(&game_sync->read_count_mutex, SEM_SCOPE_PROCS, 1);
 
   for (int i = 0; i < n_players; ++i) {
-    sem_init(&game_sync->player_may_move[i], 1, 1);
+    semaphore_create(&game_sync->player_may_move[i], SEM_SCOPE_PROCS, 1);
   }
 }
 
 void game_sync_free(game_sync_t *game_sync, uint32_t n_players) {
-  sem_destroy(&game_sync->view_should_update);
-  sem_destroy(&game_sync->view_did_update);
+  semaphore_destroy(&game_sync->view_should_update);
+  semaphore_destroy(&game_sync->view_did_update);
 
-  sem_destroy(&game_sync->master_write_mutex);
-  sem_destroy(&game_sync->game_state_mutex);
-  sem_destroy(&game_sync->read_count_mutex);
+  semaphore_destroy(&game_sync->master_write_mutex);
+  semaphore_destroy(&game_sync->game_state_mutex);
+  semaphore_destroy(&game_sync->read_count_mutex);
 
   for (int i = 0; i < n_players; ++i) {
-    sem_destroy(&game_sync->player_may_move[i]);
+    semaphore_destroy(&game_sync->player_may_move[i]);
   }
 }
