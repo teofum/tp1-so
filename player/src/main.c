2e1938e2479ea34fbd3d11d7bc7514cce0234dd8
lolo-pb <lpizzutobeltran@gmail.com>
2025-09-13 18:19:59 +0000

    fixed oopsie

--- player/src/main.c
+++ player/src/main.c
@@ -4,62 +4,6 @@
 #include <utils.h>
 
 #include <stdio.h>
-
-//#include "queue.c"///////////////////////////////////////////////////
-#include <stdio.h>
-#include <stdbool.h>
-
-#define QUEUE_CAPACITY 1000
-
-// Define a pair of integers (x, y)
-typedef struct {
-    int x;
-    int y;
-} Pair;
-
-// Queue of Pairs
-typedef struct {
-    Pair data[QUEUE_CAPACITY];
-    int front;
-    int rear;
-    int size;
-} Queue;
-
-// Initialize queue
-void initQueue4(Queue* q) {
-    q->front = 0;
-    q->rear = 0;
-    q->size = 0;
-}
-
-// Check if queue is empty
-bool isEmpty4(Queue* q) {
-    return q->size == 0;
-}
-
-// Check if queue is full
-bool isFull4(Queue* q) {
-    return q->size == QUEUE_CAPACITY;
-}
-
-// Add a Pair to the queue
-bool enqueue4(Queue* q, Pair value) {
-    if (isFull4(q)) return false;
-    q->data[q->rear] = value;
-    q->rear = (q->rear + 1) % QUEUE_CAPACITY;
-    q->size++;
-    return true;
-}
-
-// Remove a Pair from the queue
-bool dequeue4(Queue* q, Pair* out) {
-    if (isEmpty4(q)) return false;
-    *out = q->data[q->front];
-    q->front = (q->front + 1) % QUEUE_CAPACITY;
-    q->size--;
-    return true;
-}
-///////////////////////////////////////////////////////////////////////
 #include <stdlib.h>
 
 void logerr(const char *s) {
@@ -82,63 +26,6 @@ static inline int inside_rect(int x, int y, int w, int h) {
     return (x >= 0 && y >= 0 && x < w && y < h);
 }
 
-/**
- * BFS from (x, y).
- * - If any OTHER player is found on a checked tile => return 0.
- * - Otherwise return the number of passable (>0) tiles visited (area).
- */
-int bfs_area_or_player(int x, int y, game_state_t *game_state, int player_idx){ 
-
-  Pair start = { .x = x , .y =y };
-
-  int size = game_state->board_width* game_state->board_height;
-  int* visited = (int*)calloc(size, sizeof(int)); // TODO: esto podria estar en un mejor lugar
-  if (!visited) {
-    return -1;// Handle allocation failure
-  }
-
-  int area=0;
-
-  Queue q; initQueue4(&q);
-  enqueue4(&q, start);  // Add start
-  Pair pivot;
-  while (dequeue4(&q, &pivot)) { // dequeue4 to set the pivot
-    
-    for(char angle=0 ; angle < 8 ; ++angle){ // look around the pivot and enqueue4 valid
-      Pair curr = {.x = pivot.x + getX(angle), .y =  pivot.y + getY(angle)};
-      int currIndex = curr.x + (curr.y * game_state->board_width);
-      
-      for(int i=0; i<game_state->n_players; i++){ //check if player is on it
-        if( i!=player_idx && game_state->players[i].x == curr.x && game_state->players[i].y == curr.y){
-          return 0;
-        }
-      }
-      if(inBounds( curr.x, curr.y, game_state) && !visited[currIndex]){
-        enqueue4(&q, curr);
-        visited[currIndex]=1;
-        ++area;
-      }
-    }
-  
-  }
-
-  return area;  // 0 means either found a player OR no passable tiles reachable
-}
-
-char get_next_move_Mk4(game_state_t* game_state, int player_idx, char prev){
-  
-  int x = game_state->players[player_idx].x;
-  int y = game_state->players[player_idx].y;
-    
-  if(bfs_area_or_player( x, y, game_state, player_idx)){
-    // esta solo
-    return get_next_move_WallHug_L(game_state, player_idx, prev);
-  }else{
-    //si hay un playe
-    //return get_next_move_Mk3(game_state, player_idx);
-    return get_next_move_Mk2(game_state, player_idx);
-  }
-}
 
 int main(int argc, char **argv) {
   /*
