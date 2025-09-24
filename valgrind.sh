3f82f638e5074e1959ca7ddf598349d7d55b4bb2
Teo Fumagalli <teo.fum@gmail.com>
2025-09-24 16:47:46 -0300

    update valrgind sh

--- valgrind.sh
+++ valgrind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f out/valgrind.*.log
-docker exec -it -w /root/out so-builder valgrind --log-file=valgrind.%p.log --trace-children=yes --track-origins=yes --leak-check=full ./master -p ./pwall ./pblind ./pnaive ./pgreedy ./pgreedy_box ./pgreedy_w ./psgreedy ./psgreedy_box ./psgreedy_w -d 50 -v ./view -w 20 -h 15 -s 1000
+docker exec -it -w /root/out so-builder valgrind --log-file=valgrind.%p.log --trace-children=yes --track-origins=yes --leak-check=full ./master -p ./pblocker ./pblind ./pnaive ./pgreedy ./pgreedy_box ./pgreedy_w ./psgreedy ./psgreedy_box ./psgreedy_w -d 50 -v ./view -w 20 -h 15 -s 1000
